329752160b616dde2ffb463f868b186c89d166fd
Frank-197 <frankomosh197@gmail.com>
2022-07-25 11:18:33 +0300

    Done

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,4 +1,34 @@
+// import React, { useState, useEffect } from "react";
+
+// // WARNING: this useEffect will run in an infinite loop!
+// // to fix, pass an empty array as the second argument for useEffect
+// function DogPics() {
+//   const [images, setImages] = useState([]);
+
+//   useEffect(() => {
+//     console.log("useEffect");
+//     fetch("https://dog.ceo/api/breeds/image/random/3")
+//       .then((r) => r.json())
+//       .then((data) => {
+//         console.log("setState");
+//         setImages(data.message);
+//       });
+//   });
+
+//   console.log("render");
+
+//   return (
+//     <div>
+//       {images.map((image) => (
+//         <img src={image} key={image} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default DogPics;
 import React, { useState, useEffect } from "react";
+
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
@@ -6,16 +36,14 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
-
-  console.log("render");
+ 
+  }, []);
+  // console.log("render");
 
   return (
     <div>
@@ -25,5 +53,4 @@ function DogPics() {
     </div>
   );
 }
-
 export default DogPics;
